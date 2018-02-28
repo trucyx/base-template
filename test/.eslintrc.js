97fb4c3deaa764bd622505169b17db9ce0f1e161
mirreal <zgkm181142521@gmail.com>
2018-02-28 23:27:01 +0800

    fix eslintrc

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "globals": {
-        "jest": true,
-        "describe": true,
-        "expect": true,
-        "it": true,
+    globals: {
+        jest: true,
+        describe: true,
+        expect: true,
+        it: true
     }
 }
